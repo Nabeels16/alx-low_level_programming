87882899b682bbd57a35d0de791817f0a2a147c8
Nabeels16 <ibrahimnabila63@gmail.com>
2022-10-14 16:15:42 -0700

    T 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  *main -is the entry point for the program.
  *
- *Return:zero if no error, Non-zero value  if errors.
+ *Return: 0
  */
 int main(void)
 {
@@ -12,4 +12,4 @@ int main(void)
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
-e
+}
