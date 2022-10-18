b13ed0da4aade945fe88eddbb8fc93496b24173a
Nabeels16 <ibrahimnabila63@gmail.com>
2022-10-18 09:07:21 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lower case
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 'a'; n <= z; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
