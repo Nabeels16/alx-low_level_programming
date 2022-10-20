c979b3281efa05d68043f66ef4c918db016c4644
Nabeels16 <ibrahimnabila63@gmail.com>
2022-10-20 03:56:23 -0700

    Task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints is integer is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_ornegative(i);
+
+	return (0);
+}
