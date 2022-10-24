d9561247d598a7a778381f73b4a8b470927ed6ae
Nabeels16 <ibrahimnabila63@gmail.com>
2022-10-24 16:03:47 -0700

    Task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,5 +2,21 @@
 /**
  * rev_string - reverses a string
  * @s: the string
+ * Return: string in reverse
  */
 void rev_string(char *s)
+{
+	char rv = s[0];
+	int h = 0;
+	int j;
+
+	while (s[h] != '\0')
+		h++;
+	for (j = 0; j < h; j++)
+	{
+		h--;
+		rv = s[h];
+		s[j] = s[h];
+		s[h] = rv;
+	}
+}
