290bf8bc89ccc8d311f20942416f833f96c8fc1f
Nabeels16 <ibrahimnabila63@gmail.com>
2022-10-31 13:30:11 -0700

    Task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,13 +11,13 @@ void print_diagsums(int *a, int size)
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (n = 0; n < size * size; n++)
+	for (n = 0; n < (size * size); n++)
 	{
 		if (n % (size + 1) == 0)
 		{
 			sum1 += a[n];
 		}
-		if (n % (size - 1) == 0 && (n > 0 && (n < (size * size - 1))))
+		if (n % (size - 1) == 0 && n != 0 && n < size * size - 1)
 		{
 			sum2 += a[n];
 		}
